92ffeef0c9842f3261183cfc01e6aff8c23d1647
camilasatie <camilasatie.dev@gmail.com>
2020-04-26 13:12:34 -0300

    adicionado material-ui

--- src/components/Pokemones.jsx
+++ src/components/Pokemones.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
 import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import Favorite from '@material-ui/icons/Favorite';
 
 // hooks react redux
@@ -9,7 +10,21 @@ import { useDispatch, useSelector } from 'react-redux';
 // importamos a ação
 import { obtenerPokemonesAccion } from '../redux/pokeDucks';
 
+// Estilo personalizado do botão
+const useStyle = makeStyles({
+  miButton: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
+});
+
 const Pokemones = () => {
+  const classes = useStyle();
   const dispatch = useDispatch();
 
   const pokemones = useSelector((store) => store.pokemones.array);
@@ -18,18 +33,20 @@ const Pokemones = () => {
     <div>
       <Typography variant="h3">Pokemon</Typography>
       <Button
+        className={classes.miButton}
         variant="contained"
-        color="primary"
-        startIcon={<Favorite />}
+        endIcon={<Favorite />}
         onClick={() => dispatch(obtenerPokemonesAccion())}
       >
         Get Pokemons
       </Button>
-      <ul>
-        {pokemones.map((item) => (
-          <li key={item.name}>{item.name}</li>
-        ))}
-      </ul>
+      <Typography variant="body1">
+        <ul>
+          {pokemones.map((item) => (
+            <li key={item.name}>{item.name}</li>
+          ))}
+        </ul>
+      </Typography>
     </div>
   );
 };
